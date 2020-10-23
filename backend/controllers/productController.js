fdf3a6d2bfe0a55be43219f54c9e639a4092ba93
Prasad Patil <prasadrpatil25.10@gmail.com>
2020-10-23 05:51:17 +0530

    Cleanup by Using Controllers

---
+++ backend/controllers/productController.js
@@ -0,0 +1,27 @@
+import Product from '../models/productModel.js'
+import asycHandler from 'express-async-handler'
+
+// @des     Fetch All Products
+// @route   GET /api/products
+// @access  Public
+const getProducts = asycHandler(async (req, res) => {
+  const products = await Product.find({})
+  res.json(products)
+})
+
+// @des     Fetch Single Products
+// @route   GET /api/products/:id
+// @access  Public
+const getProductById = asycHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id)
+
+  if (product) {
+    res.json(product)
+  } else {
+    res.status(404)
+    throw new Error('Product Not Found')
+  }
+})
+
+
+export {getProductById,getProducts}
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,35 +1,13 @@
 import express from 'express'
-import Product from '../models/productModel.js'
-import asycHandler from 'express-async-handler'
+import {
+  getProductById,
+  getProducts,
+} from '../controllers/productController.js'
 
 const router = express.Router()
 
-// @des     Fetch All Products
-// @route   GET /api/products
-// @access  Public
-router.get(
-  '/',
-  asycHandler(async (req, res) => {
-    const products = await Product.find({})
-    res.json(products)
-  })
-)
+router.route('/').get(getProducts)
 
-// @des     Fetch Single Products
-// @route   GET /api/products/:id
-// @access  Public
-router.get(
-  '/:id',
-  asycHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id)
-
-    if (product) {
-      res.json(product)
-    } else {
-      res.status(404)
-      throw new Error('Product Not Found')
-    }
-  })
-)
+router.route('/:id').get(getProductById)
 
 export default router
