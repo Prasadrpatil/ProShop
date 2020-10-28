3418072b87be80d7757f96f2c30133081e188eb4
Prasad Patil <prasadrpatil25.10@gmail.com>
2020-10-28 16:35:34 +0530

    Show Orders On Profile

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -79,4 +79,12 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
   }
 })
 
-export { addOrderItems, getOrderById, updateOrderToPaid }
+// @desc    Get logged in User orders
+// @route   Get /api/order/myorders
+// @access  Private
+const getMyOrders = asyncHandler(async (req, res) => {
+  const orders = await Order.find({ user: req.user._id })
+  res.json(orders)
+})
+
+export { addOrderItems, getOrderById, updateOrderToPaid, getMyOrders }
