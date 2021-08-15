359a98ed62f8437b9f32b6f05f7283e5c8704818
Prasadrpatil <prasadrpatil25.10@gmail.com>
2021-08-15 11:08:51 +0530

    Product reviews added in seeder script

--- backend/seeder.js
+++ backend/seeder.js
@@ -28,6 +28,32 @@ const importData = async () => {
 
     await Product.insertMany(sampleProducts)
 
+    await Product.updateMany(
+      {},
+      {
+        $set: {
+          reviews: [
+            {
+              name: 'Aniket Rane',
+              rating: 4,
+              comment: 'Good product...',
+              user: '61189e2222cf11234c9f1111',
+              createdAt: '2021-08-15T04:58:25.162+00:00',
+              updatedAt: '2021-08-15T04:58:25.162+00:00',
+            },
+            {
+              name: 'Harshal Sutar',
+              rating: 4.5,
+              comment: 'Nice product!!!',
+              user: '61189e2222cf11234c9f1111',
+              createdAt: '2021-08-15T04:58:25.162+00:00',
+              updatedAt: '2021-08-15T04:58:25.162+00:00',
+            },
+          ],
+        },
+      }
+    )
+
     console.log('Data Imported!'.green.inverse)
     process.exit()
   } catch (error) {
